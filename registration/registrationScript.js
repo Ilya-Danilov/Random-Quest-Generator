85149c0649f1e2f1a0c14ec024f6b4066caec21e
Ilya Danilov <danilov_ia@outlook.com>
2026-03-30 20:41:17 +0300

    feat: implement main screen layout

--- registration/registrationScript.js
+++ registration/registrationScript.js
@@ -1,9 +1,12 @@
 const inputName = document.getElementById('name')
 
+
+// Проверка на то что зарегистрирован ли пользователь
 if(localStorage.getItem('AboutTheUsers') !== null){
     window.location.href = '../main/index.html'
 }
 
+// Регистрация
 window.addEventListener('submit', (e) => {
     e.preventDefault()
     const userInformation = {'name': inputName.value, 'dateRegis': new Date()}
