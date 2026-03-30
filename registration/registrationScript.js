166bff69a99d6fe0682c843e2c3e6b4ba963f730
Ilya Danilov <danilov_ia@outlook.com>
2026-03-30 17:19:50 +0300

    style: cosmetic interface edits feat: redirect authorized users from the registration page

--- registration/registrationScript.js
+++ registration/registrationScript.js
@@ -1,5 +1,9 @@
 const inputName = document.getElementById('name')
 
+if(localStorage.getItem('AboutTheUsers') !== null){
+    window.location.href = '../main/index.html'
+}
+
 window.addEventListener('submit', (e) => {
     e.preventDefault()
     const userInformation = {'name': inputName.value, 'dateRegis': new Date()}
